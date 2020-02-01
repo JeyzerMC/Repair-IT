021e4d03f30dbb2f460f5f2e9064768c47642a7f
Arnaud Paré-Vogt <arnaud.parevogt@gmail.com>
2020-01-31 23:28:53 -0500

    WIP on customers

---
+++ RepairIt/Assets/Scripts/Dropoff.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dropoff : MonoBehaviour
+{
+    [SerializeField]
+    Transform itemSpawnPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (itemSpawnPosition == null)
+        {
+            Debug.LogError("Dropoff requires an itemSpawnPosition");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void MakeCustomerArrive()
+    {
+
+    }
+}
