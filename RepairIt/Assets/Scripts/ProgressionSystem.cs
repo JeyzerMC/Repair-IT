1ecd3c936cd7b8a8db2bfff06d9e816e40f3b20f
Mehdi <jeyzermc@gmail.com>
2020-02-01 16:41:33 -0500

    progression bar

---
+++ RepairIt/Assets/Scripts/ProgressionSystem.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProgressionSystem : MonoBehaviour
+{
+
+    private float _progression = 0.0f;
+
+    private RectTransform _bar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _bar = GetComponent<RectTransform>();
+        _bar.localScale = new Vector3(Time.time, 1, 1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time < 3.0f) 
+        {
+            _bar.localScale = new Vector3(Time.time / 3.0f, 1, 1);
+        }
+    }
+}
