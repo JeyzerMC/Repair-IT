247cd7058cb804aa510bfc161e5b41fbfce0af42
Arnaud Paré-Vogt <arnaud.parevogt@gmail.com>
2020-02-01 14:14:25 -0500

    Fix player movement

--- RepairIt/Assets/Scripts/Character.cs
+++ RepairIt/Assets/Scripts/Character.cs
@@ -52,7 +52,7 @@ public class Character : MonoBehaviour
         Vector3 translation = verticalAxis + horizontalAxis;
         translation *= _currentSpeed * Time.deltaTime;
 
-        _controller.Move(translation);
+        _controller.Move(translation + Vector3.down);
 
         if (translation.magnitude != 0)
             transform.forward = translation;
