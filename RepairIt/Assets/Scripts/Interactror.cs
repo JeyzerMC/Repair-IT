30c798bff934b92793430864aeb009fd04fa68a2
Riad <combatarms970711@gmail.com>
2020-01-31 21:51:10 -0500

    We have a basic interactioon system

---
+++ RepairIt/Assets/Scripts/Interactror.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class Interactror : MonoBehaviour
+{
+    public const float MAX_RAYCAST_DISTANCE = 0.9f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            TryInteract();
+        }
+    }
+
+    void TryInteract()
+    {
+        Debug.Log("Trying to interact");
+        foreach (var hit in Physics.RaycastAll(transform.position, transform.forward, MAX_RAYCAST_DISTANCE).OrderBy(x => x.distance))
+        {
+            Debug.Log("Testing hit at distance: "+hit.distance);
+            if (hit.collider.TryGetComponent<Interactee>(out var interactee))
+            {
+                interactee.OnInteraction(this);
+                break;
+            }
+        }
+        
+    }
+}
---
+++ RepairIt/Assets/Scripts/TestInteractee.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestInteractee : MonoBehaviour, Interactee
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    public void OnInteraction(Interactror interactror)
+    {
+        Debug.Log("GOT INTERACTED WITH!");
+        Destroy(gameObject);
+    }
+}
