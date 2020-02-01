bf3026052dfd2f7de0d9e78fc567c0470ed2b45c
Arnaud Paré-Vogt <arnaud.parevogt@gmail.com>
2020-01-31 21:54:57 -0500

    Add simple input selector

---
+++ RepairIt/Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    // key, j1, j2, j3, j4
+    [SerializeField]
+    string player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(GetPlayerAxis("Horizontal"));
+    }
+
+    public float GetPlayerAxis(string name)
+    {
+        return Input.GetAxis(player + "_" + name);
+    }
+
+    public bool GetPlayerButton(string name)
+    {
+        return Input.GetButton(player + "_" + name);
+    }
+}
