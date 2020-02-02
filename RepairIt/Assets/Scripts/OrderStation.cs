db0d4d5bf76bd7129cf93c87567122c4b8382fd3
Arnaud Paré-Vogt <arnaud.parevogt@gmail.com>
2020-02-02 11:49:52 -0500

    Better box ordering

--- RepairIt/Assets/Scripts/OrderStation.cs
+++ RepairIt/Assets/Scripts/OrderStation.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Animations;
+using UnityEngine.UI;
 
 public class OrderStation : Interactee
 {
@@ -11,16 +13,29 @@ public class OrderStation : Interactee
     [SerializeField]
     Takable content;
 
+    Animator _anim;
+
     // Start is called before the first frame update
     void Start()
     {
         // Lets find the truck thrower
         _truckThrower = GameObject.FindObjectOfType<TruckThrower>();
+
+        var constraint = GetComponentInChildren<LookAtConstraint>();
+        ConstraintSource source = new ConstraintSource();
+        source.weight = 1;
+        source.sourceTransform = Camera.main.transform;
+        constraint.SetSources(new List<ConstraintSource> { source });
+
+        var text = GetComponentInChildren<Text>();
+        text.text = content.tag;
+
+        _anim = GetComponentInChildren<Animator>();
     }
 
     public override void OnInteraction(Interactror interactror)
     {
-        Debug.Log("Bruh");
+        _anim.SetTrigger("Order");
         _truckThrower.ThrowBox(boxPrefab.GetComponent<Rigidbody>()).GetComponent<DeliveryboxBehaviour>().content = content;
     }
 }
